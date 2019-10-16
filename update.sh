f327bd5f0000fb1faf993ed2d28a3053fe3f615a
domichal <domichal@o2.pl>
2019-10-16 14:43:25 +0200

    * modified update file to work with "mylauncher" folder

--- update.sh
+++ update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd /home/cpi/launcher 
-feh --bg-center /home/cpi/launcher/sys.py/gameshell/wallpaper/updating.png
+cd /home/cpi/mylauncher 
+#feh --bg-center /home/cpi/mylauncher/sys.py/gameshell/wallpaper/updating.png
 git pull
 git reset --hard $1
 git submodule init
 git submodule update
 
-cd ~/apps/Menu && git pull && cd -
+#cd ~/apps/Menu && git pull && cd -
 
-feh --bg-center /home/cpi/launcher/sys.py/gameshell/wallpaper/loading.png 
+feh --bg-center /home/cpi/mylauncher/sys.py/gameshell/wallpaper/loading.png 
 ./load.sh
 
