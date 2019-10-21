21f5cb7da5aeae23d31b75b3f6d2fef478748da4
Michal <domichal@o2.pl>
2019-10-22 00:00:45 +0200

    * readded missing comment to myconfig * readded misssing battery icon

--- sys.py/myconfig.py
+++ sys.py/myconfig.py
@@ -1,5 +1,8 @@
-# The values below would make the launcher behave/look like the original
-# 
+# ADDMENU_PATH - Path to the secondary menu folder, feel free to move it outside the launcher folder
+# DEFAULT_FOCUSED_ITEM - Item to be highlighted by default on page render
+#
+# The values below will make the launcher behave/look like original
+#
 # ADDMENU_PATH = "/home/cpi/apps/Menu"
 # DEFAULT_FOCUSED_ITEM = 1
 
